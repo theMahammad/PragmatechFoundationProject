2e51bba666bac903262a548cf08e770c31492a81
theMahammad <hssjxie@gmail.com>
2021-02-05 21:01:12 +0400

    My Project

--- My_Project/js/script.js
+++ My_Project/js/script.js
@@ -1,43 +1,58 @@
-
 let nav = $('.nav');
 let scroll;
-$(window).scroll( function(e){
+$(window).scroll(function (e) {
     scroll = $(window).scrollTop();
-    if(scroll>130){
-        
-        
-         nav.css('width','100%');
-        nav.css("border-radius","0px")
-    }
-        if(scroll<=130){
+    if (scroll > 130) {
 
-        
-        nav.css("width" , "1000px");
-        nav.css("border-radius","50px")  
+
+        nav.css('width', '100%');
+        nav.css("border-radius", "0px")
+    }
+    if (scroll <= 130) {
+
+
+        nav.css("width", "1000px");
+        nav.css("border-radius", "50px")
     }
 
-    
+
 })
 
 
 
 var listItem = $('.slider-item-list > li')
-function display(arg, arr) {
-  $(arg).html(arr[0]);
 
-  var i = 1;
-var myVar = setInterval(function() {
-    $(arg).fadeOut(50, 'linear', function() {
-      $(arg).append(arr[i]).fadeIn(100);
-      i++;
-    });
-    if(i==3){
-        clearInterval(myVar);
-    }
-    
-  }, 1000);
-    
-    
+//function display(arg, arr) {
+//    $(arg).html(arr[0]);
+//
+//    var i = 1;
+//    var myVar = setInterval(function () {
+//        $(arg).fadeOut(500, 'linear', function () {
+//            $(arg).append(arr[i]).fadeIn(500);
+//            i++;
+//        });
+//        if (i == 3) {
+//            clearInterval(myVar);
+//        }
+//
+//    }, 1000);
+//
+//
+//}
+//display($('.slider-item-list'), listItem);
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var sliderItems = document.getElementsByClassName("slider-item");
+ 
+  for (i = 0; i < sliderItems.length; i++) {
+    sliderItems[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex >=  sliderItems.length) {slideIndex = 1}    
+  
+  sliderItems[slideIndex-1].style.display = "block";  
+ 
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-display($('.slider-item-list'), listItem);
-
