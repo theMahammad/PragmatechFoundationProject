9558429ef5124e303bd9cc99237c4dcbe8b700e9
theMahammad <hssjxie@gmail.com>
2021-02-07 13:36:54 +0400

    My Project

--- My_Project/js/script.js
+++ My_Project/js/script.js
@@ -2,6 +2,7 @@ let nav = $('.nav');
 let scroll;
 $(window).scroll(function (e) {
     scroll = $(window).scrollTop();
+    console.log(scroll);
     if (scroll > 130) {
 
 
@@ -19,40 +20,17 @@ $(window).scroll(function (e) {
 })
 
 
+let images= ["img/header.jpg","img/header-2.jpg"];
+let slides = $('.slider-item img');
 
-var listItem = $('.slider-item-list > li')
+let prevArrow = document.querySelector('.prev-arrow');
+let nxtArrow = $('.next-arrow');
 
-//function display(arg, arr) {
-//    $(arg).html(arr[0]);
-//
-//    var i = 1;
-//    var myVar = setInterval(function () {
-//        $(arg).fadeOut(500, 'linear', function () {
-//            $(arg).append(arr[i]).fadeIn(500);
-//            i++;
-//        });
-//        if (i == 3) {
-//            clearInterval(myVar);
-//        }
-//
-//    }, 1000);
-//
-//
-//}
-//display($('.slider-item-list'), listItem);
-var slideIndex = 0;
-showSlides();
-function showSlides() {
-  var i;
-  var sliderItems = document.getElementsByClassName("slider-item");
- 
-  for (i = 0; i < sliderItems.length; i++) {
-    sliderItems[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex >=  sliderItems.length) {slideIndex = 1}    
-  
-  sliderItems[slideIndex-1].style.display = "block";  
- 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+
+
+
+
+
+
+
+
