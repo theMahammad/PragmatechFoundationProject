065b09d4c932ff2555fbb46ab63f022b1cd78151
theMahammad <hssjxie@gmail.com>
2021-03-14 19:49:56 +0400

    Almost The Part of Front End of My Project is finished :)

--- My_Project/app.py
+++ My_Project/app.py
@@ -1,8 +1,9 @@
 from flask import Flask,request,render_template,redirect
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQL_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/data.db'
 db = SQLAlchemy(app)
+
 @app.route("/")
 def homePage():
     return render_template("app/home-page.html")
