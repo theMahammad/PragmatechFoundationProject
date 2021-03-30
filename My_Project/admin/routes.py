68cb441842246b0b88e276ba63213390eeba6e39
theMahammad <hssjxie@gmail.com>
2021-03-30 09:19:55 +0400

    Front səhifələr extend-blok halına çevrildi

--- My_Project/admin/routes.py
+++ My_Project/admin/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template,Blueprint
-admin_bp = Blueprint('admin',__name__,template_folder='templates')
+admin_bp = Blueprint('adminPanel',__name__,template_folder='templates',static_folder='static',static_url_path='/static/admin')
 
 @admin_bp.route("/adminside")
 def index():
